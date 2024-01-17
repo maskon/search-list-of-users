400d87be2899acfabe863425fa334205a6d453eb
Максим Блинов <126991331+maskon@users.noreply.github.com>
2024-01-17 17:32:48 +0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,12 +1,9 @@
 const list = document.getElementById('list')
 const filterElem = document.getElementById('filter')
-const container = document.querySelector('.container')
-
-container.classList.remove('w-50')
 
 async function start() {
     try {
-        const url = await fetch('https://jsonplaceholder.typicode.com/posts')
+        const url = await fetch('https://jsonplaceholder.typicode.com/comments')
         const user = await url.json()
         
         list.innerHTML = 'Loading...'
@@ -21,16 +18,15 @@ async function start() {
             
         },2000)   
         
-        filterElem.addEventListener('input', () => {
-                    
-            const userFilter = user.filter(function (arr) {
-
-                return arr.title.startsWith(filterElem.value)
-                
+        filterElem.addEventListener('input', () => { 
+            
+            const userFilter = user.filter(function (arr) {     
+                const value = filterElem.value.toUpperCase()
+                return arr.name.toUpperCase().includes(value)      
             })
             
             list.innerHTML = ''
-
+            
             userFilter.forEach(function (item) {                
                 render(item)
             })
@@ -40,7 +36,8 @@ async function start() {
         })
         
     } catch (err) {
-        console.log('error')
+        list.style.color = 'red'
+        list.innerHTML = err.message
     }    
 }
 
@@ -48,7 +45,7 @@ start()
 
 function render(item) {
     const li = document.createElement('li')
-    li.innerHTML = `<li class="list-group-item">${item.title}</li>`
+    li.innerHTML = `<li class="list-group-item">${item.name}</li>`
     li.style.listStyleType = 'none'
     list.appendChild(li)
 }
